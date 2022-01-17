a63172fec8902442d59790fd110e95409c8e09dc
tusharc31 <tushar.choudhary@research.iiit.ac.in>
2022-01-17 23:14:07 +0530

    Completed

---
+++ Assignment-0/webcam.py
@@ -0,0 +1,22 @@
+import cv2
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,360)
+cap.set(10,100) # Brightness id is 10
+
+cnt = 0
+while True:
+    success, img = cap.read()
+    if success == True:
+        cnt+=1
+        cv2.imshow("output",img)
+        cv2.imwrite("Results/Q2/frame"+str(cnt)+".jpg", img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+print(f"\nSaved {cnt} frames to Results/Q2")
